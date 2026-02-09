5872ff86f6619de6e90f0b318c59a06eebe452ad
Sridharselvakumar <144665171+Sridharselvakumar@users.noreply.github.com>
2026-02-09 22:11:22 +0530

    Add files via upload

---
+++ day3_python.py
@@ -0,0 +1,48 @@
+###.......add two numbers...........
+##a=int(input("enter the number:"))
+##b=int(input("enter the number:"))
+##print(a+b)
+##print("the addition of two number is :",a+b)
+
+
+##a=int(input("enter the number:"))
+##b=int(input("enter the number:"))
+##print("squre of a and b",(a**b))
+##print("agine squre of the value a and b",(a**b)**2)
+
+##x=int(input("enter the number:"))
+##print("the squre of the number is:",x**2)
+##print(f"the squre of the {x} is {x**2}")
+
+
+##a=float(input("enter the float number:"))
+##c=int(a)
+##print(f"the int vaule of {c}")
+
+#......PRINT THE LAST CHAR OF STR.....
+
+##a="sridhar"
+##print(f"last char of {a} is  {a[-1]}")
+
+##a=eval(input("enter the name:"))
+##print(f"last char of {a} is {a[-1]}")
+
+
+
+##.........REVERSE THE STRING........
+
+##a=eval(input("enter the name:"))
+##print(f"reversed name of {a} is {a[::-1]}")
+
+
+
+##........get the middle values......
+##a=eval(input("enter the name:"))
+##print(f"the middle value of {a} is {a[len(a)//2]}")
+
+##..........extract the last number.........
+a=eval(input("enter the name:"))
+print(f"last value of {a} is {[0(-1)])"}
+      
+
+
