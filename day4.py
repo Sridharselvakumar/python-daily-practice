96834a53a70b1a616c3959f5f1928524a9fd2fba
Sridharselvakumar <144665171+Sridharselvakumar@users.noreply.github.com>
2026-02-10 22:03:26 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,29 @@
+##s=eval(input("enter the data:"))
+##if len(s)==5:
+##    print(f"{s} has exactly 5 characters"
+
+#...... 
+##n=eval(input("enter the number:"))
+##if n%5==0:
+##    print(f"{n} cube is {n**3
+
+# WRITE A PROGRAMME TO CHECK WHEATHER THE GIVEN INT IS MULTIPLE OF 5 AND DIVISIBLE OF 3
+##n=eval(input("enter the number:"))
+##if (n%5)==0 and(n%3)==0:
+##    print(f"{n} is divide by 3 and multible by 5")
+
+
+#write a program to check wheather the given number is 2 digit number or not
+
+##n=int(input("enter the number"))
+##if (n<=99 and n>=10):
+##    print(f"{n} is a two digit number")
+
+
+#...write a program to check weather the enter char is UPPERCASE OR LOWERCASE
+
+##n=eval(input("enter the letter"))
+##if 'A'<=n<='Z':
+##    print(f"{n} is a upper case")
+    
+WRITE LOWERCASE
