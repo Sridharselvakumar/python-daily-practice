555b5e2b054bc7762951df5e868e599d7d129ee6
Sridharselvakumar <144665171+Sridharselvakumar@users.noreply.github.com>
2026-02-11 23:00:20 +0530

    Add files via upload

---
+++ day5 python.py
@@ -0,0 +1,37 @@
+## >>>>>>>>>>>>>>>>>>>>>>>>ELIF<<<<<<<<<<<<<<<<<<<<<<<
+
+##a=int(input("enter the value:"))
+##if (a>=90):
+##    print("medical college")
+##elif a>=80:
+##    print("engneering college")
+##elif a>=60:
+##    print("bca,bsc")
+##elif a>=35:
+##    print("arts")
+##else:
+##    print(" u are fail")
+
+##** 1 elif ....  check relationship between two numbers.....
+
+##a=int(input("enter the value a:"))
+##b=int(input("enter the value b:"))
+##if a<b:
+##    print(f"entered value of {a} is smaller than {b}")
+##elif a>b:
+##    print(f"entered value of {a} is greater than {b}")
+##else:
+##    print(f"entered value of {a} and {b} both are equal")
+
+
+##**......concider 2 codinates that is x and y and which quadrants data points are present.....
+
+##a=int(input("enter the value a:"))
+##b=int(input("enter the value b:"))
+##if a>=0 and b>=0:
+##    print(f"{a} and {b} meets in 1st quadrants")
+##elif a>=0 and b<0:
+##    print(f"{a} and {b} meets in 2nd quadrants")
+##elif a<0 and b<0:
+##    print(f"{a} and {b} meets in 3rd quadrants")
+##elif a<0 and b>=0:
