a4bb9f9ccca2ce7dd57322a4a0d6103026099e05
Sridharselvakumar <144665171+Sridharselvakumar@users.noreply.github.com>
2026-02-12 22:34:00 +0530

    Add files via upload

---
+++ day6-python.py
@@ -0,0 +1,83 @@
+##......write a program to check weather the given number is single digit or
+##......2 digit or 3 gigit or more than 3 digit nymber.........
+
+
+##a=abs(int(input("enter the value a:")))    #>>> abs inbuilt function covert negative number into possitive
+##if a>=0 and a<=9:
+##    print(f"{a} is single digit value")
+##elif a>=10 and a<=99:
+##    print(f"{a} is double digit value")
+##elif a<=100 and a>=999:
+##    print(f"{a} is 3 digit value")
+##else:
+##    print(f"{a} is greater than 3 digit")
+
+
+#.........write a program to cheack weather the given character is uppercase
+#or lowercase or digits or special character...........
+
+##a=(input("enter the data"))
+##if 'A'<=a<='Z':
+##    print(F"{a} is uppercase char")
+##elif 'a'<=a<='z':
+##    print(f"{a} is lowercase char")
+##elif '0'<=a<='9':
+##    print(f"{a} is a digit")
+##else:
+##    print(f"{a} is special char")
+
+
+##........predict the states of the student result based on the student score
+
+##a=eval(input("enter the mark"))
+##if 60<=a<=84:
+##    print("first class")
+##elif 85<=a<=100:
+##    print("distriction")
+##elif 59>=a>=35:
+##    print("second class")
+##elif 35>a>=0:
+##    print("fail")
+##else:
+##    print("invalid")
+
+
+#>............write a pro.. the greatest number among the 4 number
+
+##a=int(input("enter the 1st number :"))
+##b=int(input("enter the 2nd number :"))
+##c=int(input("enter the 3rd number :"))
+##d=int(input("enter the 4th number :"))
+##if a>b and a>c and a>d:
+##    print(" a is greater")                    
+##elif b>a and b>c and b>d:
+##    print("b is greater")
+##elif c>a and c>b and c>d:
+##    print("c is greater")
+##elif d>a and d>b and d>c:
+##    print("d is greater")
+
+###FULL FORM >>^^
+
+##a=int(input("enter the 1st number :"))
+##b=int(input("enter the 2nd number :"))
+##c=int(input("enter the 3rd number :"))
+##d=int(input("enter the 4th number :"))
+##if a>b and a>c and a>d:
+##    print(" a is greater")
+##elif b>c and b>d:
+##    print("b is greater")
+##elif c>d:
+##    print("c is greater")
+##else:
+##    print("d is greater")
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> NESTED IF <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+##CONDITION INSIDE THE CONDITION
+
+
+
+
+   
